ab5587458b0d1f3fe83cc7a73a5ab4c30ed252c0
eugene <lonfort@mail.ru>
2019-04-14 16:21:27 +0300

    Tasks added, comments removed

--- main.cpp
+++ main.cpp
@@ -2,9 +2,6 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
 
 #include "ip_filter.cpp"
 
@@ -12,84 +9,25 @@ int main()
 {
   try
   {
-
     IpPool ip_pool = get_input();
 
     std::sort(ip_pool.begin(), ip_pool.end(), ip_adressees_comparator);
 
-    for (auto const &ip_address : ip_pool)
-    {
-      bool first = true;
-      for (auto const &ip_address_part : ip_address)
-      {
-        std::cout << (!first ? "." : "") << ip_address_part;
-        first = false;
-      }
-      std::cout << std::endl;
-    }
+    output(ip_pool);
 
-    // 222.173.235.246
-    // 222.130.177.64
-    // 222.82.198.61
-    // ...
-    // 1.70.44.170
-    // 1.29.168.152
-    // 1.1.234.8
+    // TODO filter by first byte and output   // ip = filter(1)
+    IpPool ip_filtered = filter(ip_pool, std::string("1"));
+    std::sort(ip_filtered.begin(), ip_filtered.end(), ip_adressees_comparator);
+    output(ip_filtered);
 
-    // TODO filter by first byte and output
-    // ip = filter(1)
+    // TODO filter by first and second bytes and output   // ip = filter(46, 70)
+    ip_filtered = filter(ip_pool, std::string("46"), std::string("70"));
+    std::sort(ip_filtered.begin(), ip_filtered.end(), ip_adressees_comparator);
+    output(ip_filtered);  
 
-    // 1.231.69.33
-    // 1.87.203.225
-    // 1.70.44.170
-    // 1.29.168.152
-    // 1.1.234.8
-
-    // TODO filter by first and second bytes and output
-    // ip = filter(46, 70)
-
-    // 46.70.225.39
-    // 46.70.147.26
-    // 46.70.113.73
-    // 46.70.29.76
-
-    // TODO filter by any byte and output
-    // ip = filter_any(46)
-
-    // 186.204.34.46
-    // 186.46.222.194
-    // 185.46.87.231
-    // 185.46.86.132
-    // 185.46.86.131
-    // 185.46.86.131
-    // 185.46.86.22
-    // 185.46.85.204
-    // 185.46.85.78
-    // 68.46.218.208
-    // 46.251.197.23
-    // 46.223.254.56
-    // 46.223.254.56
-    // 46.182.19.219
-    // 46.161.63.66
-    // 46.161.61.51
-    // 46.161.60.92
-    // 46.161.60.35
-    // 46.161.58.202
-    // 46.161.56.241
-    // 46.161.56.203
-    // 46.161.56.174
-    // 46.161.56.106
-    // 46.161.56.106
-    // 46.101.163.119
-    // 46.101.127.145
-    // 46.70.225.39
-    // 46.70.147.26
-    // 46.70.113.73
-    // 46.70.29.76
-    // 46.55.46.98
-    // 46.49.43.85
-    // 39.46.86.85
-    // 5.189.203.46
+    // TODO filter by any byte and output     // ip = filter_any(46)
+    ip_filtered = filter_any(ip_pool, std::string("46"));
+    output(ip_filtered);
   }
   catch (const std::exception &e)
   {
