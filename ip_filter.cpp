05c3c2ac20d7d7a21e619ff2a04da7ae892b83ba
eugene <lonfort@mail.ru>
2019-04-14 16:22:40 +0300

    filter and filter_any functions implemented, output function added, sort ordetr changed

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -37,19 +37,69 @@ bool ip_address_parts_comparator(const std::string& left_part, const std::string
 {
   if (left_part.size() < right_part.size())
   {
-    return true;
+    return false;
   }
   else if (left_part.size() > right_part.size())
   {
-    return false;
+    return true;
   }
 
-  return left_part < right_part;
+  return left_part > right_part;
 }
 
 bool ip_adressees_comparator(const IpAddress& left_address, const IpAddress& right_address)
 {
   return std::lexicographical_compare(left_address.begin(), left_address.end(), right_address.begin(), right_address.end(), ip_address_parts_comparator);
+}
+
+template<typename... Args>
+IpPool filter(const IpPool& ip_pool, Args... args)
+{
+  std::string filter_expression;
+  __attribute__((unused)) int arr[sizeof...(args)]  = {(filter_expression.append(args + "."), 0)...};
+  filter_expression.pop_back();  //remove last dot
+
+  IpPool ip_pool_filtered;
+  IpAddress ip_address_to_compare = split(filter_expression, '.');
+
+  for (auto const &ip_address : ip_pool)
+  {
+    if (std::equal(ip_address_to_compare.begin(), ip_address_to_compare.end(), ip_address.begin()))
+    {
+      ip_pool_filtered.push_back(ip_address);
+    }
+  }
+
+  return ip_pool_filtered;
+}
+
+IpPool filter_any(const IpPool& ip_pool, const std::string& filter_expression)
+{
+  IpPool ip_pool_filtered;
+  for (auto const &ip_address : ip_pool)
+  {
+    if (std::find_if(ip_address.begin(), ip_address.end(), 
+    [filter_expression](const std::string &str) { return str == filter_expression; }) != ip_address.end())
+    {
+      ip_pool_filtered.push_back(ip_address);
+    }
+  }
+
+  return ip_pool_filtered;
+}
+
+void output(const IpPool &ip_pool)
+{
+  for (auto const &ip_address : ip_pool)
+  {
+    bool first = true;
+    for (auto const &ip_address_part : ip_address)
+    {
+      std::cout << (!first ? "." : "") << ip_address_part;
+      first = false;
+    }
+    std::cout << std::endl;
+  }
 }
 
 IpPool get_input(std::istream &is);
