403fdab840f53963e84441df3436b5665c4610e5
eugene <lonfort@mail.ru>
2019-04-16 18:24:44 +0300

    Tasks updated according to new functions semantic. Excess sort calls removed

--- main.cpp
+++ main.cpp
@@ -11,22 +11,20 @@ int main()
   {
     IpPool ip_pool = get_input();
 
-    std::sort(ip_pool.begin(), ip_pool.end(), ip_adressees_comparator);
+    std::sort(ip_pool.begin(), ip_pool.end(), std::greater<std::vector<int>>());
 
     output(ip_pool);
 
     // TODO filter by first byte and output   // ip = filter(1)
-    IpPool ip_filtered = filter(ip_pool, std::string("1"));
-    std::sort(ip_filtered.begin(), ip_filtered.end(), ip_adressees_comparator);
+    IpPool ip_filtered = filter(ip_pool, 1);
     output(ip_filtered);
 
     // TODO filter by first and second bytes and output   // ip = filter(46, 70)
-    ip_filtered = filter(ip_pool, std::string("46"), std::string("70"));
-    std::sort(ip_filtered.begin(), ip_filtered.end(), ip_adressees_comparator);
+    ip_filtered = filter(ip_pool, 46, 70);
     output(ip_filtered);  
 
     // TODO filter by any byte and output     // ip = filter_any(46)
-    ip_filtered = filter_any(ip_pool, std::string("46"));
+    ip_filtered = filter_any(ip_pool, 46);
     output(ip_filtered);
   }
   catch (const std::exception &e)
