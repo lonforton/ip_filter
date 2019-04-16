c5d1687dc3f7d0548b4e00fd3324d47849a4ca38
eugene <lonfort@mail.ru>
2019-04-16 18:26:02 +0300

    Tests updated accroding to new functions calls semantics

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -13,11 +13,8 @@ inline std::ostream &operator<<(std::ostream &os, std::vector<T> const &v)
   for (auto const &element : v)
   {
     os << (!first ? "." : "");
-    for (auto const &item : element)
-    {
-      os << item;
-      first = false;
-    }
+    os << element;
+    first = false;
   }
   os << endl;
   return os;
@@ -29,7 +26,7 @@ BOOST_AUTO_TEST_SUITE(ip_filter_test_suite)
 BOOST_AUTO_TEST_CASE(ip_filter_input_test)
 {
   std::stringstream ss;
-  ss << std::string("4.5.66.7") << std::endl << std::string("6.74.8.9") << std::endl << std::string("5.7.254.0");
+  ss << std::string("4.5.66.7") << std::endl << std::string("6.74.8.9 5 6") << std::endl << std::string("5.7.254.0 1");
 
   IpPool ip_pool = get_input(ss);
 
@@ -40,14 +37,14 @@ BOOST_AUTO_TEST_CASE(ip_filter_input_test)
 BOOST_AUTO_TEST_CASE(ip_filter_sort_test)
 {
   std::stringstream ss;
-  ss << std::string("1.231.69.33") << std::endl << std::string("1.29.168.152") << std::endl << std::string("1.70.144.170") << std::endl 
-     << std::string("1.87.203.225") << std::endl << std::string("1.1.234.8") << std::endl;
+  ss << std::string("1.11.69.33 1 1") << std::endl << std::string("1.101.1.1") << std::endl << std::string("1.70.144.70 0") << std::endl 
+     << std::string("1.87.203.225") << std::endl << std::string("1.3.234.8 0 111") << std::endl;
 
-  IpPool ip_pool = get_input(ss);
+  IpPool ip_pool = get_input(ss);  
 
-  std::sort(ip_pool.begin(), ip_pool.end(), ip_adressees_comparator);
+  std::sort(ip_pool.begin(), ip_pool.end(), std::greater<std::vector<int>>());
 
-  IpPool expected_sorted_ip_pool {{"1","231","69","33"}, {"1","87","203","225"}, {"1","70","144","170"}, {"1","29","168","152"}, {"1","1","234","8"}};
+  std::vector<std::vector<int>> expected_sorted_ip_pool { {1, 101, 1, 1}, {1,87,203,225}, {1,70,144,70}, {1, 11, 69, 33}, {1,3,234,8} };
 
   BOOST_CHECK_EQUAL_COLLECTIONS(ip_pool.begin(), ip_pool.end(), expected_sorted_ip_pool.begin(), expected_sorted_ip_pool.end());
 }
@@ -55,17 +52,15 @@ BOOST_AUTO_TEST_CASE(ip_filter_sort_test)
 BOOST_AUTO_TEST_CASE(ip_filter_filter_1_test)
 {
   std::stringstream ss;
-  ss << std::string("1.5.46.7") << std::endl << std::string("4.50.6.8") << std::endl << std::string("1.5.6.99") << std::endl << std::string("3.46.60.7");
+  ss << std::string("1.5.46.7") << std::endl << std::string("4.50.6.8 111") << std::endl << std::string("1.5.6.99") << std::endl << std::string("3.46.60.7") << std::endl << std::string("2.1.1.1") << std::endl;
 
   IpPool ip_pool = get_input(ss);
 
-  std::sort(ip_pool.begin(), ip_pool.end(), ip_adressees_comparator);
+  std::sort(ip_pool.begin(), ip_pool.end(), std::greater<std::vector<int>>());
 
-  IpPool filtered_ip_pool = filter(ip_pool, std::string("1"));
+  IpPool filtered_ip_pool = filter(ip_pool, 1);
 
-  std::sort(filtered_ip_pool.begin(), filtered_ip_pool.end(), ip_adressees_comparator);
-
-  IpPool expected_filtered_ip_pool { {"1","5","46","7"}, {"1","5","6","99"},};
+  IpPool expected_filtered_ip_pool { {1, 5, 46, 7}, {1, 5, 6, 99} };
 
   BOOST_CHECK_EQUAL_COLLECTIONS(filtered_ip_pool.begin(), filtered_ip_pool.end(), expected_filtered_ip_pool.begin(), expected_filtered_ip_pool.end());
 }
@@ -73,17 +68,15 @@ BOOST_AUTO_TEST_CASE(ip_filter_filter_1_test)
 BOOST_AUTO_TEST_CASE(ip_filter_filter_2_test)
 {
   std::stringstream ss;
-  ss << std::string("1.5.46.7") << std::endl << std::string("4.50.6.8") << std::endl << std::string("1.46.6.99") << std::endl << std::string("3.46.60.7");
+  ss << std::string("1.5.46.7") << std::endl << std::string("4.50.6.8 0 0") << std::endl << std::string("1.46.6.99") << std::endl << std::string("3.46.60.7") << std::endl;
 
   IpPool ip_pool = get_input(ss);
 
-  std::sort(ip_pool.begin(), ip_pool.end(), ip_adressees_comparator);
+  std::sort(ip_pool.begin(), ip_pool.end(), std::greater<std::vector<int>>());
 
-  IpPool filtered_ip_pool = filter(ip_pool, std::string("1"), std::string("46"));
+  IpPool filtered_ip_pool = filter(ip_pool, 1, 46);
 
-  std::sort(filtered_ip_pool.begin(), filtered_ip_pool.end(), ip_adressees_comparator);
-
-  IpPool expected_filtered_ip_pool { {"1","46","6","99"}};
+  IpPool expected_filtered_ip_pool { {1,46, 6, 99} };
 
   BOOST_CHECK_EQUAL_COLLECTIONS(filtered_ip_pool.begin(), filtered_ip_pool.end(), expected_filtered_ip_pool.begin(), expected_filtered_ip_pool.end());
 }
@@ -91,15 +84,15 @@ BOOST_AUTO_TEST_CASE(ip_filter_filter_2_test)
 BOOST_AUTO_TEST_CASE(ip_filter_filter_any_test)
 {
   std::stringstream ss;
-  ss << std::string("4.5.46.7") << std::endl << std::string("4.5.6.8") << std::endl << std::string("406.5.146.99") << std::endl << std::string("3.46.6.7");
+  ss << std::string("4.5.46.7") << std::endl << std::string("4.5.6.8 1 0") << std::endl << std::string("406.5.146.99 1 0") << std::endl << std::string("3.46.6.7") << std::endl;
 
   IpPool ip_pool = get_input(ss);
 
-  std::sort(ip_pool.begin(), ip_pool.end(), ip_adressees_comparator);
+  std::sort(ip_pool.begin(), ip_pool.end(), std::greater<std::vector<int>>());
 
-  IpPool filtered_ip_pool = filter_any(ip_pool, std::string("46"));
+  IpPool filtered_ip_pool = filter_any(ip_pool, 46);
 
-  IpPool expected_filtered_ip_pool { {"4","5","46","7"}, {"3","46","6","7"}};
+  IpPool expected_filtered_ip_pool { {4, 5, 46, 7}, {3, 46, 6, 7}};
 
   BOOST_CHECK_EQUAL_COLLECTIONS(filtered_ip_pool.begin(), filtered_ip_pool.end(), expected_filtered_ip_pool.begin(), expected_filtered_ip_pool.end());
 }
